4cde7a4f9ffded6d4a1495099b16db79b16841ed
Fernando H. Sanches <fernandohsanches@gmail.com>
2018-08-21 02:57:19 -0300

    rewriting for simplicity(???)

--- klein.py
+++ klein.py
@@ -2,7 +2,7 @@
 
 from itertools import chain, imap
 
-arcs = {}
+INFTY = float('inf')
 
 
 class Arc:
@@ -17,7 +17,7 @@ class Arc:
             self.mate = self.create_mate()
         else:
             self.mate = mate
-        arcs[(s, t)] = self
+        s.arcs_dict[(s.label, t.label)] = self
 
     def create_mate(self):
         mate_label = find_mate(self.label)
@@ -26,33 +26,42 @@ class Arc:
 
     def euler_visit(self):
         ls = [self.label]
-        ls.extend(self.t.E())
+        ls.extend(self.t.euler_visit())
         ls.append(self.mate.label)
+        # ls.extend(self.t.E())
+        # ls.append(self.mate.label)
         return ls
 
 
 class Node:
-    def __init__(self, label):
+    def __init__(self, label, tree_root=None):
         self.children = []
         self.arcs = []
         self.label = label
         self.weigth = 1
         self.euler = None
+        self.root = None
+        if(not tree_root):
+            self.arcs_dict = {}
+        else:
+            self.arcs_dict = tree_root.arcs_dict
 
     def __iter__(self):
         for v in chain(*imap(iter, self.children)):
             yield v
         yield self
 
-    def euler_string_compute(self):
+    def euler_list_compute(self):
         res = []
         for arc in self.arcs:
-            res.extend(arc.euler_visit())
+            res.append(arc.label)
+            res.extend(arc.t.euler_list_compute())
         return res
 
     def E(self):
         if(not self.euler):
-            self.euler = self.euler_string_compute()
+            el = self.euler_list_compute()
+            self.euler = Euler_String(el)
         return self.euler
 
     def add_child(self, child):
@@ -77,8 +86,14 @@ class Node:
 
         return heavier_child
 
-    def post_processing(self):
+    def post_processing(self, root):
         self.calculate_weigth()
+        self.set_roots(self)
+
+    def set_roots(self, root):
+        self.root = root
+        for child in self.children:
+            child.set_roots(root)
 
     def heavy_path(self):
         path = [self]
@@ -89,30 +104,62 @@ class Node:
 
         return path
 
-    def difference_sequence(self, path=None):
+    def difference_sequence(self, E, pos, path):
         if(not self.children):  # is empty
             return []
         if(not path):
             path = self.heavy_path()
         else:
+            (st, ed) = pos
 
             v = path[1]  # self's child in P
 
-            p = arcs[(self, v)]
+            p = self.arcs_dict[(self, v)]
             q = p.mate
 
-            E = self.E()
+            rv = E.arcs[p.label]
+            vr = E.arcs[q.label]
 
-            T_left = list_split(E, p.label)[0] + [p.label]  # ending with p
-            T_right = [q.label] + list_split(E, q.label)[1]  # starting with q
+            T_left = E[st:rv]  # ending with p
+            T_right = E[vr:ed]  # starting with p
+
+            print(p.label)
+            print(q.label)
+            print("rv = " + str(rv) + " vr = " + str(vr))
+            print("T_left = " + str(T_left))
+            print("T_right = " + str(T_right))
+
+            # T_left = list_split(E, p.label)[0] + [p.label]  # ending with p
+            # T_right = [q.label] +list_split(E, q.label)[1]  # starting with q
 
             p = v.heavy_path()
-            res = T_left + T_right[::-1] + v.difference_sequence(p)
+            res = T_left + T_right[::-1] + v.difference_sequence(
+                E, pos, path)
+
+            self.difference_seq = res
 
             return res
 
-    def difference_symbol(self):
-        self.difference_sequence()[0]
+    def set_diff_dict(self):
+        diff = {}
+        euler = self.root.E()
+        i = 1
+        j = len(euler)
+
+        seq = self.difference_sequence()
+
+        for label in seq:
+            if(label == euler[i]):
+                diff[(i, j)] = 0
+                i += 1
+            elif(label == euler[j]):
+                diff[(i, j)] = -1
+                j -= 1
+            else:
+                raise Exception("Bad difference sequence")
+
+            euler.diff_dict = diff
+        # a
 
     def special_subtrees(self):
         hpath = self.heavy_path()
@@ -132,64 +179,41 @@ class Euler_String():
     Overrides access methods to the string
     '''
     def __init__(self, string):
-        self._string = string
+        self.string = string
 
-        self.arcs = [None] * (len(string)+1)
+        self.arcs = [None] * (len(string)*20+1)  # fixme esse *2 e' gambiarra
+        print("str= " + str(string))
+        print(self.arcs)
 
-        for (index, character) in enumerate(self._string):
-            self.arcs[character] = index
+        for (index, value) in enumerate(self.string):
+            print(value)
+            self.arcs[value] = index
 
         self.start = 0
         self.end = len(string)
 
-    @property
-    def string(self):
-        # print("i,j= " + str(self.start) + "," + str(self.end))
-        return self._string  # [self.start:self.end]
+        # self._difference_darts = {}
+        self.diff_dict = {}
+        self.diff_seq = None
 
-    def __contains__(self, element):
-        return(self.arcs[element])
-
-    def __getitem__(self, index):
-        return self._string[index]
-
-    # def __setitem__(self, index, new):
-    #     self.string[index] = new
-    #     return self.string[index]
+    def get_pos(self):
+        return(self.start, self.end)
 
     def __str__(self):
         return "<str: " + str(self.string) + ">"
 
-    def index_of(self, item):
-        return self.arcs[item]
+    def __getitem__(self, index):
+        return self.string[index]
 
-    def is_empty(self):
-        return self.start == self.end
-
-    def substring(self, start, end):
-        sub_start = self.start + start
-        sub_end = self.start + end
-        return Euler_Substring(self, sub_start, sub_end)
-
-    def diffence_symbol(self):
-        raise Exception("NYI")
-
-    # vetor        abs   relativo
-    # [0, 1, 2, 3] end=4 end=end
-    # [0, 1, 2]    end=3 end=parent.end -1
-    #    [1, 2]    end=3 end = parent.end
-    def remove(self, symbol):
-        '''returns new substring without the symbol iff symbol is at an end'''
-        print("removing " + str(symbol) + " from " + str(self.string))
-        print("with i,j= " + str(self.start) + "," + str(self.end))
-        if(self._string[self.start] == symbol):
-            return Euler_Substring(self, self.start+1, self.end)
-        elif(self._string[self.end-1] == symbol):
-            return Euler_Substring(self, self.start, self.end-1)
-        else:
-            text = "BadRemoval: " + str(symbol) + " from " + str(self.string)
-            raise(Exception(text))
-        return None
+    def diffence_symbol(self, pos):
+        (start, end) = pos
+        try:
+            x = self.diff_dict[(start, end)]
+        except Exception:
+            print("dict=" + str(self.diff_dict))
+            x = None
+            raise(Exception("BadDictAccess"))
+        return x
 
     def has_mate(self, symbol):
         return(symbol*(-1) in self.string)
@@ -205,8 +229,8 @@ class Euler_String():
         (tpp_start, tpp_end) = (self.start+1, mate_index)
         (tp_start, tp_end) = (mate_index + 1, self.end)
 
-        tpp = Euler_Substring(self, tpp_start, tpp_end)
-        tp = Euler_Substring(self, tp_start, tp_end)
+        tpp = (tpp_start, tpp_end)
+        tp = (tp_start, tp_end)
 
         return(e, tpp, e_m, tp)
 
@@ -217,61 +241,66 @@ class Euler_String():
         (tp_start, tp_end) = (self.start, mate_index)
         (tpp_start, tpp_end) = (mate_index + 1, self.end-1)
 
-        tpp = Euler_Substring(self, tpp_start, tpp_end)
-        tp = Euler_Substring(self, tp_start, tp_end)
+        tpp = (tpp_start, tpp_end)
+        tp = (tp_start, tp_end)
 
         return(tp, e_m, tpp, e)
 
+    def build_diff_dict(self, pos):
+        if(not self.diff_seq):
+            self.difference_sequence()
 
-class Euler_Substring(Euler_String):
-    def __init__(self, parent, start, end):
-        self._string = parent._string
-        self.parent = parent
-        self.start = start
-        self.end = end
+        i = self.start
+        j = self.end - 1
 
-    @property
-    def string(self):
-        return self._string[self.start:self.end]
+        for element in self.difference_seq:
+            element_pos = self.arcs[element]
+            if(element_pos == i):
+                self.diff_dict[(i, j)] = True
+            elif(element_pos == j):
+                self.diff_dict[(i, j)] = False
+            else:
+                raise Exception("Bad difference sequence")
 
-    def __getitem__(self, index):
-        print(self._string)
-        print("i=" + str(index) +
-              ", start= " + str(self.start) +
-              ", end= " + str(self.end))
-
-        if(index >= 0):
-            return self._string[index+self.start]
+    def remove(self, pos, symbol):
+        '''returns new substring without the symbol iff symbol is at an end'''
+        (start, end) = pos
+        if(self[start] == symbol):
+            return (start+1, end)
+        elif(self[end-1] == symbol):
+            return (start, end-1)
         else:
-            # index = index * -1
-            # index = index + 1
-            return self._string[index + self.end]
+            text = "BadRemoval: " + str(symbol) + " from " + str(self)
+            raise(Exception(text))
+        return None
 
+    def substring(self, parent, start, end):
+        sub_start = parent.start + start
+        sub_end = parent.start + end
+        return (sub_start, sub_end)
 
-def generate_relevant_substrings(self, F):
-    pass
-
-
-INFTY = float('inf')
+    def is_empty(self, pos):
+        (st, ed) = pos
+        return (st == ed)
 
 
 class Klein():
-    def delete_from_t(self, s, t):
-        if(t.is_empty()):
+    def delete_from_t(self, s, s_pos, t, t_pos):
+        if(t.is_empty(t_pos)):
             return INFTY
-        e = t.diffence_symbol()
+        e = t.diffence_symbol(t_pos)
         if(t.has_mate(e)):
-            return(self.dist(s, t.remove(e)) + self.cdel(e, t))
+            return(self.dist(s, t.remove(t_pos, e)) + self.cdel(e, t))
         else:
-            return(self.dist(s, t.remove(e)))
+            return(self.dist(s, t.remove(t_pos, e)))
 
-    def delete_from_s(self, s, t):
-        if(s.is_empty()):
+    def delete_from_s(self, s, s_pos, t, t_pos):
+        if(s.is_empty(s_pos)):
             return INFTY
-        if(t.is_empty()):
+        if(t.is_empty(t_pos)):
             e = s[-1]
         else:
-            if(t.diffence_symbol() == t[-1]):
+            if(t.diffence_symbol(t_pos) == t[-1]):
                 e = s[-1]
             else:
                 e = s[0]
@@ -280,11 +309,11 @@ class Klein():
         else:
             return self.dist(s.remove(e), t)
 
-    def match(self, s, t):
+    def match(self, s, s_pos, t, t_pos):
         print("match s=" + str(s) + " t=" + str(t))
-        if(s.is_empty() and t.is_empty()):
+        if(s.is_empty(s_pos) and t.is_empty(t_pos)):
             return 0
-        if(s.is_empty() or t.is_empty()):
+        if(s.is_empty(s_pos) or t.is_empty(t_pos)):
             return INFTY
         e = t.diffence_symbol()
         if(e == t[0]):
@@ -327,7 +356,9 @@ def list_split(ls, x):
         return [ls[0:i], ls[i+1:]]
 
 
-def is_empty(tup):
-    '''input: a tuple'''
-    print(tup)
-    return tup[0] == tup[1]
+#def is_empty(tup):
+#    '''input: a tuple'''
+#    print(tup)
+#    return tup[0] == tup[1]
+
+
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from klein import Node, Euler_String, Euler_Substring, Klein, list_split
+from klein import Node, Euler_String, Klein
 import unittest
 
 
@@ -25,7 +25,7 @@ def create_tree():
     b.add_child(d)
     d.add_child(e)
 
-    a.post_processing()
+    a.post_processing(a)
 
     return a
 
@@ -43,16 +43,16 @@ def create_tree_b():
     #         k
 
     a = Node(1)
-    b = Node(2)
-    c = Node(3)
-    d = Node(4)
-    e = Node(5)
-    f = Node(6)
-    g = Node(7)
-    h = Node(8)
-    i = Node(9)
-    j = Node(10)
-    k = Node(11)
+    b = Node(2, a)
+    c = Node(3, a)
+    d = Node(4, a)
+    e = Node(5, a)
+    f = Node(6, a)
+    g = Node(7, a)
+    h = Node(8, a)
+    i = Node(9, a)
+    j = Node(10, a)
+    k = Node(11, a)
     a.add_child(b)
 
     a.add_child(c)
@@ -65,7 +65,7 @@ def create_tree_b():
     g.add_child(j)
     j.add_child(k)
 
-    a.post_processing()
+    a.post_processing(a)
 
     return a
 
@@ -83,16 +83,16 @@ def create_tree_c():
     #            k
 
     a = Node(1)
-    b = Node(2)
-    c = Node(3)
-    d = Node(4)
-    e = Node(5)
-    f = Node(6)
-    g = Node(7)
-    h = Node(8)
-    i = Node(9)
-    j = Node(10)
-    k = Node(11)
+    b = Node(2, a)
+    c = Node(3, a)
+    d = Node(4, a)
+    e = Node(5, a)
+    f = Node(6, a)
+    g = Node(7, a)
+    h = Node(8, a)
+    i = Node(9, a)
+    j = Node(10, a)
+    k = Node(11, a)
 
     a.add_child(b)
     a.add_child(c)
@@ -105,22 +105,29 @@ def create_tree_c():
     g.add_child(j)
     j.add_child(k)
 
+    a.post_processing(a)
+
     return a
 
 
-tree = create_tree()
+def create_tree_singleton():
+        a = Node(1)
+        a.post_processing(a)
+        return a
 
 
 class TestSuite(unittest.TestCase):
 
     k = Klein()
 
-    def test_list_split(self):
-        ls = [1, 2, 3, 4]
-        self.assertEqual(list_split(ls, 3), [[1, 2], [4]])
-        self.assertEqual(list_split(ls, 2), [[1], [3, 4]])
+    # def test_list_split(self):
+    #     ls = [1, 2, 3, 4]
+    #     self.assertEqual(list_split(ls, 3), [[1, 2], [4]])
+    #     self.assertEqual(list_split(ls, 2), [[1], [3, 4]])
 
     def test_arc(self):
+        tree = create_tree()
+
         if(len(tree.arcs) == 0):
             return
 
@@ -133,35 +140,35 @@ class TestSuite(unittest.TestCase):
 
     def test_euler(self):
         a = create_tree()
-        # self.assertEqual(a.E(), "bdeEDBcC")
-        self.assertEqual(a.E(), [2, 4, 5, -5, -4, -2, 3, -3])
-        # self.assertEqual(a.children[0].E(), "deED")
-        self.assertEqual(a.children[0].E(), [4, 5, -5, -4])
-        self.assertEqual(a.children[1].E(), [])
 
-    def test_string_index(self):
-        a = Euler_String([2, -3, 3, -2, 4, -4, 5, -5])
-        self.assertEqual(a[0], 2)
-        self.assertEqual(a[1], -3)
-        self.assertEqual(a[-1], -5)
+        self.assertEqual(a.E().string, [2, 4, 5, -5, -4, -2, 3, -3])
+        self.assertEqual(a.children[0].E().string, [4, 5, -5, -4])
+        self.assertEqual(a.children[1].E().string, [])
 
-        #                            [ 0,  1, 2,  3, 4,  5, 6,  7]
-        #                            [-8, -7,-6, -5,-4, -3,-2, -1]
-        #                parent is:  [ 2, -3, 3, -2, 4, -4, 5, -5]
-        b = a.substring(1, 5)  # #[-3, 3, -2, 4]
-        self.assertEqual(b[0], -3)
-        self.assertEqual(b[1], 3)
-        self.assertEqual(b[-1], 4)
+    # def test_string_index(self):
+    #     euler_string_a = Euler_String([2, -3, 3, -2, 4, -4, 5, -5])
+    #     self.assertEqual(a[0], 2)
+    #     self.assertEqual(a[1], -3)
+    #     self.assertEqual(a[-1], -5)
 
-        c = a.substring(1, 8)  # [-3, 3, -2, 4, -4, 5, -5]
-        self.assertEqual(c[0], -3)
-        self.assertEqual(c[1], 3)
-        self.assertEqual(c[-1], -5)
+    #     #                            [ 0,  1, 2,  3, 4,  5, 6,  7]
+    #     #                            [-8, -7,-6, -5,-4, -3,-2, -1]
+    #     #                parent is:  [ 2, -3, 3, -2, 4, -4, 5, -5]
+    #     b = a.substring(a_str, 1, 5)  # #[-3, 3, -2, 4]
+    #     self.assertEqual(b[0], -3)
+    #     self.assertEqual(b[1], 3)
+    #     self.assertEqual(b[-1], 4)
 
-        d = c.substring(1, 5)  # [3, -2, 4, -4]
-        self.assertEqual(d[0], 3)
-        self.assertEqual(d[1], -2)
-        self.assertEqual(d[-1], -4)
+    #     c = a.substring(a_str1, 8)  # [-3, 3, -2, 4, -4, 5, -5]
+    #     c_str = c.get_pos()
+    #     self.assertEqual(c[0], -3)
+    #     self.assertEqual(c[1], 3)
+    #     self.assertEqual(c[-1], -5)
+
+    #     d = c.substring(c_str, 1, 5)  # [3, -2, 4, -4]
+    #     self.assertEqual(d[0], 3)
+    #     self.assertEqual(d[1], -2)
+    #     self.assertEqual(d[-1], -4)
 
     def test_weights(self):
         a = create_tree()
@@ -179,9 +186,16 @@ class TestSuite(unittest.TestCase):
 
     def test_difference_sequence(self):
         a = create_tree()
-        # expected = "bCcBdDeE"
         expected = [2, -3, 3, -2, 4, -4, 5, -5]
-        self.assertEqual(a.difference_sequence(a.heavy_path()), expected)
+        self.assertEqual(a.difference_sequence(a.E(),
+                                               (0, 0),
+                                               a.heavy_path()),
+                         expected)
+
+    def test_dict_constructors(self):
+        a = Node(1)
+        b = Node(2, a)
+        self.assertEqual(a.arcs_dict, b.arcs_dict)
 
     def test_special_subtrees(self):
 
@@ -256,6 +270,10 @@ class TestSuite(unittest.TestCase):
         result = map(lambda x: x.label, result)
         self.assertEqual(result, expected)
 
+    def test_multiple_difference_sequences(self):
+        c = create_tree_c()
+        c.special_subtrees()
+
     def test_string_removal(self):
         # a = Euler_String("abcd", "adcb")
         # b = Euler_String("a", "a")
@@ -266,11 +284,19 @@ class TestSuite(unittest.TestCase):
         # self.assertRaises(Exception, a.remove, 'c')
 
         a = Euler_String([1, 2, 3, 4])
+        a_pos = a.get_pos()
         b = Euler_String([1])
-        self.assertEqual(a.remove(1).string, [2, 3, 4])
-        self.assertEqual(a.remove(4).string, [1, 2, 3])
-        self.assertEqual(a.remove(1).remove(4).string, [2, 3])
-        self.assertTrue(b.remove(1).is_empty())
+        b_pos = b.get_pos()
+
+        (res1s, res1e) = a.remove(a_pos, 1)
+        (res1sb, res1eb) = a.remove(a_pos, 4)
+        (res2s, res2e) = a.remove((res1s, res1e), 4)
+        res_b = b.remove(b_pos, 1)
+
+        self.assertEqual(a[res1s:res1e], [2, 3, 4])
+        self.assertEqual(a[res1sb:res1eb], [1, 2, 3])
+        self.assertEqual(a[res2s:res2e], [2, 3])
+        self.assertTrue(b.is_empty(res_b))
         self.assertRaises(Exception, a.remove, 3)
 
     def test_is_empty(self):
@@ -279,58 +305,54 @@ class TestSuite(unittest.TestCase):
         # c = b.remove("a")
 
         a = Euler_String([])
+        a_str = a.get_pos()
         b = Euler_String([1])
-        c = b.remove(1)
+        b_str = b.get_pos()
+        c_str = b.remove(b_str, 1)
 
-        self.assertTrue(a.is_empty())
-        self.assertFalse(b.is_empty())
-        self.assertTrue(c.is_empty())
+        self.assertTrue(a.is_empty(a_str))
+        self.assertFalse(b.is_empty(b_str))
+        self.assertTrue(b.is_empty(c_str))
 
     def test_split_first(self):
-        # a = Euler_String("bcdaDCBA", "bABcCDda")
-
-        # expected_e = ("b", "B")
-        # expected_res_str = ("cdaDC", "A")
-        # expected_res_diff = ("cCDda", "A")
-
-        # (e, res1, em, res2) = a.split_first("b", True)
-
         a = Euler_String([2, 3, 4, 1, -4, -3, -2, -1])
 
         expected_e = (2, -2)
         expected_tp_tpp_str = ([3, 4, 1, -4, -3], [-1])
 
-        (e, tpp, em, tp) = a.split_first(2, True)
+        (e, (st1, ed1), em, (st2, ed2)) = a.split_first(2, True)
+
+        str1 = a[st1:ed1]
+        str2 = a[st2:ed2]
 
         self.assertEqual((e, em), expected_e)
-        self.assertEqual((tpp.string, tp.string), expected_tp_tpp_str)
+        self.assertEqual((str1, str2), expected_tp_tpp_str)
 
     def test_split_last(self):
-        # a = Euler_String("bcdaDCBA", "AbBcCDda")
-
-        # expected_e = ("A", "a")
-        # expected_res_str = ("bcd", "DCB")
-        # expected_res_diff = ("", "bBcCDd")
-
-        # (res1, em, res2, e) = a.split_last("A", True)
-
         a = Euler_String([2, 3, 4, 1, -4, -3, -2, -1])
 
         expected_e = (-1, 1)
         expected_res_str = ([2, 3, 4], [-4, -3, -2])
 
-        (res1, em, res2, e) = a.split_last(-1, True)
+        ((st1, ed1), em, (st2, ed2), e) = a.split_last(-1, True)
+        ls1 = a[st1:ed1]
+        ls2 = a[st2:ed2]
 
         self.assertEqual((e, em), expected_e)
-        self.assertEqual((res1.string, res2.string), expected_res_str)
+        self.assertEqual((ls1, ls2), expected_res_str)
 
     def test_match(self):
         k = Klein()
         a = Euler_String([1, 2, 3, 4])
+        a_pos = a.get_pos()
         b = Euler_String([])
+        b_pos = b.get_pos()
         c = Euler_String([])
-        self.assertEqual(k.match(b, c), 0)
-        self.assertEqual(k.match(a, b), k.match(a, c))
+        c_pos = c.get_pos()
+
+        self.assertEqual(k.match(b, b_pos, c, c_pos), 0)
+        self.assertEqual(k.match(a, a_pos, b, b_pos),
+                         k.match(a, a_pos, c, c_pos))
 
     def test_remove_from_s(self):
         k = Klein()
@@ -342,44 +364,41 @@ class TestSuite(unittest.TestCase):
         # empty = Euler_String(0, 0)
 
         a = Euler_String([1])
+        a_pos = a.get_pos()
         b = Euler_String([-1, 1])
+        b_pos = b.get_pos()
         c = Euler_String([-1, 1])
+        c_pos = c.get_pos()
         e = Euler_String([1, 2, 3, 4, -4, -3, -2, -1])
+        e_pos = e.get_pos()
         f = Euler_String([1, 2, 3, 4, -4, -3])
+        f_pos = f.get_pos()
         empty = Euler_String([])
+        empt_pos = empty.get_pos()
 
-        self.assertEqual(k.delete_from_s(empty, empty), float('inf'))
-        self.assertEqual(k.delete_from_s(a, empty), 0)
-        self.assertEqual(k.delete_from_s(b, empty), 1)
-        self.assertEqual(k.delete_from_s(c, empty), 1)
-        self.assertEqual(k.delete_from_s(e, empty), 4)
-        self.assertEqual(k.delete_from_s(f, empty), 2)
-        self.assertEqual(k.delete_from_s(e, f), 2)
-        pass
+        self.assertEqual(k.delete_from_s(empty, e_pos, empty, e_pos),
+                         float('inf'))
+        self.assertEqual(k.delete_from_s(a, a_pos, empty, empt_pos), 0)
+        self.assertEqual(k.delete_from_s(b, b_pos, empty, empt_pos), 1)
+        self.assertEqual(k.delete_from_s(c, c_pos, empty, empt_pos), 1)
+        self.assertEqual(k.delete_from_s(e, e_pos, empty, empt_pos), 4)
+        self.assertEqual(k.delete_from_s(f, f_pos, empty, empt_pos), 2)
+        self.assertEqual(k.delete_from_s(e, e_pos, f, f_pos), 2)
 
     def test_remove_from_t(self):
         k = Klein()
-        # a = Euler_String("a", "a")
-        # b = Euler_String("Aa", "Aa")
-        # c = Euler_String("Aa", "aA")
-        # e = Euler_String("abcdDCBA", "AaBbCcdD")
-        # f = Euler_String("abcdDC", "abcdDC")
-        # empty = Euler_String("", "")
 
-        a = Euler_String([1])
-        b = Euler_String([-1, 1])
-        c = Euler_String([-1, 1])
-        e = Euler_String([1, 2, 3, 4, -4, -3, -2, -1])
-        f = Euler_String([1, 2, 3, 4, -4, -3])
-        empty = Euler_String([])
+        a = create_tree().E()
+        b = create_tree_b().E()
+        bp = create_tree_b().E()
+        c = create_tree_c().E()
+        one = create_tree_singleton().E()
 
-        self.assertEqual(k.delete_from_t(empty, empty), float('inf'))
-        self.assertEqual(k.delete_from_t(empty, a), 0)
-        self.assertEqual(k.delete_from_t(empty, b), 1)
-        self.assertEqual(k.delete_from_t(empty, c), 1)
-        self.assertEqual(k.delete_from_t(empty, e), 4)
-        self.assertEqual(k.delete_from_t(empty, f), 2)
-        self.assertEqual(k.delete_from_t(f, e), 2)
+        self.assertEqual(k.delete_from_t(one, one), float('inf'))
+        self.assertEqual(k.delete_from_t(one, a), 5)
+        self.assertEqual(k.delete_from_t(one, b), 11)
+        self.assertEqual(k.delete_from_t(one, bp), 11)
+        self.assertEqual(k.delete_from_t(one, c), 11)
         pass
 
     def test_has_mate(self):
@@ -402,7 +421,6 @@ class TestSuite(unittest.TestCase):
         self.assertFalse(a.has_mate(-4))
         self.assertFalse(a.has_mate(5))
         self.assertFalse(a.has_mate(6))
-
 
 
 #   a
