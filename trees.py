fe615cd76d90de51d3bdadf552b35984ebbc7b5f
Fernando H. Sanches <fernandohsanches@gmail.com>
2018-08-23 18:32:13 -0300

    added trees file

---
+++ trees.py
@@ -0,0 +1,38 @@
+A
+#   a
+#  / \
+# b   c
+# |
+# d
+# |
+# e
+
+# bdeEDBcC
+# 01234567
+
+B
+#   a
+#  / \
+# b   c
+# |  / \
+# d f   g
+# | |  / \
+# e h i   j
+#         |
+#         k
+
+# bdeEDBcfhHFgiIjkKJGC
+# 01234567890123456780
+
+#     a
+#    /  \
+#   b    c
+#  / \  / \
+# d   ef   g
+# |       / \
+# h      i   j
+#            |
+#            k
+
+# [bdhHDeEBcfFgiIjkKJGC]
+# [01234567890123456780]
